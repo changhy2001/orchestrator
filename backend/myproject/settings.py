8dce7dcda65c531c8fda25a5f4d3310dd2df85b3
HaYoung Chang <changhy2001@HaYoungs-MacBook-Pro.local>
2025-03-09 13:52:03 +0900

    delete cors

--- backend/myproject/settings.py
+++ backend/myproject/settings.py
@@ -50,18 +50,14 @@ INSTALLED_APPS = [
     'search',
     'channels', # Channels 추가
     'rest_framework',
-    'corsheaders',
 ]
 
-CORS_ALLOW_CREDENTIALS = True
-CORS_ALLOWED_ORIGINS = ["http://localhost:8081"]  # We add your frontend URL here.
-CSRF_TRUSTED_ORIGINS = ['http://localhost:8081']  # We add your frontend URL here.
+CSRF_TRUSTED_ORIGINS = ['http://localhost:8081']
 
 # ASGI 애플리케이션 경로 설정
 ASGI_APPLICATION = 'myproject.asgi.application'
 
 MIDDLEWARE = [
-    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
